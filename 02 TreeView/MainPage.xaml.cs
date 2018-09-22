275ed375ab00cdc9b774adb83f11b398ba760bf5
French Bear <FrenchBear38@outlook.com>
2018-09-22 11:24:08 +0200

    TreeView example

---
+++ 02 TreeView/MainPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿// TreeView
+// Learning UWP
+// 2018-09-22   PV
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TreeView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ViewModel vm;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            vm = new ViewModel(this);
+            DataContext = vm;
+
+            BlocksTreeView.RootNodes.Add(vm.BlocksRoot);
+        }
+    }
+
+
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private readonly MainPage w;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string property) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(property)));
+
+
+        public ViewModel(MainPage w)
+        {
+            this.w = w;
+            BlocksRecordsList = UniBlocks.BlockRecords.Values.OrderBy(br => br.Rank).ToList();
+
+            BlocksRoot = new BlockNode("TopLevel", 4);
+            foreach (var l3 in BlocksRecordsList.GroupBy(b => b.Level3Name).OrderBy(g => g.Key))
+            {
+                var r3 = new BlockNode(l3.Key, 3);
+                BlocksRoot.Children.Add(r3);
+                foreach (var l2 in l3.GroupBy(b => b.Level2Name))
+                {
+                    var r2 = new BlockNode(l2.Key, 2);
+                    r3.Children.Add(r2);
+                    foreach (var l1 in l2.GroupBy(b => b.Level1Name))
+                    {
+                        // Special case, l1 can be empty
+                        TreeViewNode r1;
+                        if (l1.Key.Length > 0)
+                        {
+                            r1 = new BlockNode(l1.Key, 1);
+                            r2.Children.Add(r1);
+                        }
+                        else
+                            r1 = r2;
+                        // Blocks
+                        foreach (var l0 in l1)
+                        {
+                            var node = new BlockNode(l0.BlockName, 0);
+                            r1.Children.Add(node);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public List<BlockRecord> BlocksRecordsList { get; set; }
+
+        public TreeViewNode BlocksRoot { get; set; }
+    }
+
+
+    internal class BlockNode : TreeViewNode
+    {
+
+        public BlockNode(string name, int level)
+        {
+            Name = name;
+            Level = level;
+            Content = $"{level}: {name}";
+            IsExpanded = true;
+        }
+
+        public int Level { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 02 TreeView/UniBlocks.cs
@@ -0,0 +1,112 @@
+﻿// UniBlocks
+// Hierarchical example data for TreeView example
+// 2018-09-22   PV
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TreeView
+{
+    public class BlockRecord
+    {
+        public int Begin { get; private set; }
+        public int End { get; private set; }
+        public string BlockName { get; private set; }
+        public string Level1Name { get; private set; }
+        public string Level2Name { get; private set; }
+        public string Level3Name { get; private set; }
+
+        // Sorting order matching hierarchy
+        public int Rank { get; internal set; }
+
+        public string BlockNameAndRange => $"{BlockName} {Begin:X4}..{End:X4}";
+
+
+        internal BlockRecord(int Begin, int End, string BlockName, string Level1Name, string Level2Name, string Level3Name)
+        {
+            this.Begin = Begin;
+            this.End = End;
+            this.BlockName = BlockName;
+            this.Level1Name = Level1Name;
+            this.Level2Name = Level2Name;
+            this.Level3Name = Level3Name;
+        }
+
+        public override string ToString()
+        {
+            return $"BlockRecord(Range={Begin:X4}..{End:X4}, Block={BlockName}, L1={Level1Name}, L2={Level2Name}, L3={Level3Name})";
+        }
+    }
+
+
+    static class UniBlocks
+    {
+        // Real internal dictionaries used to store Unicode data
+        private static readonly Dictionary<int, BlockRecord> block_map = new Dictionary<int, BlockRecord>();
+
+        public static ReadOnlyDictionary<int, BlockRecord> BlockRecords => new ReadOnlyDictionary<int, BlockRecord>(block_map);
+
+        static UniBlocks()
+        {
+            // Read blocks
+            using (var sr = new StreamReader(GetResourceStream("MetaBlocks.txt")))
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (line.Length == 0 || line[0] == '#') continue;
+                    string[] fields = line.Split(';');
+                    string[] field0 = fields[0].Replace("..", ";").Split(';');
+                    int begin = int.Parse(field0[0], NumberStyles.HexNumber);
+                    int end = int.Parse(field0[1], NumberStyles.HexNumber);
+                    BlockRecord br = new BlockRecord(begin, end, fields[1], fields[2], fields[3], fields[4]);
+                    block_map.Add(begin, br);
+                }
+
+            // Compute rank using an integer of format 33221100 where 33 is index of L3 block, 22 index of L2 block in L3...
+            int rank3 = 0;
+            foreach (var l3 in block_map.Values.GroupBy(b => b.Level3Name).OrderBy(g => g.Key))
+            {
+                int rank2 = 0;
+                foreach (var l2 in l3.GroupBy(b => b.Level2Name))
+                {
+                    int rank1 = 0;
+                    foreach (var l1 in l2.GroupBy(b => b.Level1Name))
+                    {
+                        int rank0 = 0;
+                        foreach (var l0 in l1)
+                        {
+                            l0.Rank = rank0 + 100 * (rank1 + 100 * (rank2 + 100 * rank3));
+                            rank0++;
+                        }
+                        rank1++;
+                    }
+                    rank2++;
+                }
+                rank3++;
+            }
+
+        }
+
+
+        // Returns stream from embedded resource name
+        private static Stream GetResourceStream(string name)
+        {
+            name = "." + name;
+            var assembly = typeof(UniBlocks).GetTypeInfo().Assembly;
+            var qualifiedName = assembly.GetManifestResourceNames().FirstOrDefault(n => n.EndsWith(name, StringComparison.OrdinalIgnoreCase));
+            if (qualifiedName == null)
+                return null;
+            else
+                return assembly.GetManifestResourceStream(qualifiedName);
+        }
+
+    }
+}
